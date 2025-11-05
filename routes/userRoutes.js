3a7271cd73cbdd18dee612374cb1bb6a12f8c08e
Bhavana <bhavanar991@gmail.com>
2025-11-05 19:49:39 +0530

    Fix: Correct case for UserController filename

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser, getUserProfile, updateUserProfile } = require("../controllers/UserController");
+const { registerUser, loginUser, getUserProfile, updateUserProfile } = require("../controllers/userController");
 
 const { protect } = require("../middleware/authmiddleware");
 
