55d33c0bea256a3d138687c4460cd7ba91272092
Bhavana <bhavanar991@gmail.com>
2025-11-09 18:36:54 +0530

    Add multer-cloudinary config

---
+++ config/multer-cloudinary.js
@@ -0,0 +1,15 @@
+const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("./cloudinary");
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "job_portal_resumes",
+    resource_type: "raw"
+  }
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;
