4b7e092ba2a95380539d92b65d7c0d73bc1bf275
Bhavana <bhavanar991@gmail.com>
2025-11-10 21:10:27 +0530

    Fix multer-cloudinary import path

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { protect } = require("../middleware/authmiddleware");
-const upload = require("../config/multer");
+const upload = require("../config/multer-cloudinary");
+
 
 const {
   createApplication,
